823e3f4697d2cb349d5e59943d12170006755ad4
Ariel Campos <ariel.campos94@yahoo.com>
2023-10-16 11:14:32 -0300

    created sign in hook

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -4,18 +4,43 @@ import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { ButtonText } from '../../components/ButtonText';
 
+import { useAuth } from '../../hooks/auth';
+import { useState } from 'react';
+
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { signIn } = useAuth();
+
+    function handleSignIn(){
+        signIn({email, password})
+    }
+
     return(
         <Container>
             <Form>
                 <h1>RocketMovies</h1>
                 <p>Aplicação para acompanhar tudo que assistir.</p>
-
                 <h2>Faça seu login</h2>
-                <Input icon={FiMail} placeholder="E-mail" type="email"/>
-                <Input icon={FiLock} placeholder="Senha" type="password"/>
 
-                <Button title="Entrar"/>
+                <Input 
+                    icon={FiMail} 
+                    placeholder="E-mail" 
+                    type="email"
+                    onChange={event => setEmail(event.target.value)}
+                />
+
+                <Input 
+                    icon={FiLock} 
+                    placeholder="Senha" 
+                    type="password"
+                    onChange={event => setPassword(event.target.value)}
+                />
+
+                <Button
+                    title="Entrar"
+                    onClick={handleSignIn}
+                />
 
                 <ButtonText 
                     title="Criar conta"
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
 import { Container, Form, Background } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { ButtonText } from '../../components/ButtonText';
+import { api } from '../../services/api';
 
 export function SignUp(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function handleSignUp(){
+        if(!name.length || !email.length || !password.length)
+            return alert("Preencha todos os campos obrigatórios!");
+
+        await api
+            .post("/users", {name, email, password})
+            .then(() => {
+                alert("Usuário cadastrado com sucesso!");
+                navigate("/");
+            })
+            .catch(error =>{
+                if(error.response){
+                    alert(error.response.data.message);
+                }
+                else{
+                    alert("Não foi possível cadastrar!");
+                }
+            });
+    }
+
     return(
         <Container>
             <Form>
@@ -12,11 +42,26 @@ export function SignUp(){
                 <p>Aplicação para acompanhar tudo que assistir.</p>
 
                 <h2>Crie a sua conta</h2>
-                <Input icon={FiUser} placeholder="Nome"/>
-                <Input icon={FiMail} placeholder="E-mail" type="email"/>
-                <Input icon={FiLock} placeholder="Senha" type="password"/>
+                <Input 
+                    icon={FiUser} 
+                    placeholder="Nome"
+                    onChange={event=>setName(event.target.value)}
+                    />
+                <Input 
+                    icon={FiMail} 
+                    placeholder="E-mail" type="email"
+                    onChange={event=>setEmail(event.target.value)}
+                    />
+                <Input 
+                    icon={FiLock} 
+                    placeholder="Senha" type="password"
+                    onChange={event=>setPassword(event.target.value)}
+                />
 
-                <Button title="Cadastrar"/>
+                <Button 
+                    title="Cadastrar"
+                    onClick={handleSignUp}
+                />
 
                 <span>
                     <FiArrowLeft/>
